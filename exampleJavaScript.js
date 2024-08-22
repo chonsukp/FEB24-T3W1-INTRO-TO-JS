d3db273f843c19c4d037a41852c89e26f80cfcb5
chonsukp <chonsukp@gmail.com>
2024-08-22 21:03:24 +1000

    hello world

---
+++ exampleJavaScript.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+
+
+
+
+let someMessage = "bananas";
+
+console.log(someMessage);
+
+someMessage = "Over 9000";
+
+console.log(someMessage);
+
+
+
+
+
+
+
+console.log("Hello Again World!");
