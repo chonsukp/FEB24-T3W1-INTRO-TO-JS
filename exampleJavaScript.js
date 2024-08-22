397c6d7682c58a2e1e9189bebf366305a504ed8a
chonsukp <chonsukp@gmail.com>
2024-08-22 22:25:39 +1000

    week 1 day 2 class content

--- exampleJavaScript.js
+++ exampleJavaScript.js
@@ -1,21 +1,91 @@
 console.log("Hello World");
 
+// if condition
+let someMessage = "Bananas";
 
+if (someMessage) {
+    console.log("Conditional log");
+}
+// loose equality
+if (someMessage == "oranges") {
+    console.log("We have oranges");
+}
 
+// strick equality 
+let someNumberMessage = "10";
 
+if (someNumberMessage === 10) {
+    console.log("We have a number 10!")
+}
 
-let someMessage = "bananas";
+// greater than or equal to
+if (someNumberMessage >= 5) {
+    console.log("Numer is greater than 5");
+}
 
-console.log(someMessage);
+// ternary / simplified if else
+let someCoolMessage = someMessage === "oranges" ? someMessage.substring(0,2).toLocaleUpperCase() : "Default message here";
+//                                                "Bananas"  .  "ba"        .      "BA"
+console.log(someCoolMessage);
 
-someMessage = "Over 9000";
+// block scope - 'let' keyword is for local scope
+if (someMessage == "oranges") {
+    let someCoolMessage = someMessage.substring(0,2).toLocaleUpperCase() + "inside the if statement";
+    console.log(someCoolMessage);
+} else {
+    let someCoolMessage = "Default message here inside the else statement ";
+    console.log(someCoolMessage, someCoolMessage, someCoolMessage);
+}
 
-console.log(someMessage);
+console.log("Some cool message is now: " + someCoolMessage);
 
+// swith case 
+let fruitChoices = ["oranges", "bananas", "magoes"];
 
+let fruitChoicesAsObject = {
+    oranages: "oranges",
+    bananas: "bananas",
+    mangoes: "mangoes"
+}
 
+let fruit = fruitChoicesAsObject.bananas;
 
+switch (fruit) {
+    case fruitChoicesAsObject.bananas:
+        console.log("We have bananas!");
+        break;
+    case fruitChoicesAsObject.oranges:
+        console.log("We have oranges!");
+        break;
+    default:
+        console.log("We have some exotic fruits!")
+        break;
+}
 
+someMessage = "bananas";
+switch (someMessage) {
+    case "oranges":
+    console.log("We have oranges");
+    break;
+    case "bananas":
+    console.log("We have bananas");
+    break;
+    case "magoes":
+    console.log("We have mangoes");
+    break;
+    default:
+    console.log("We some exotic fruits");
+    break;
+}
 
+// short circut 
+someCoolMessage = false;
 
-console.log("Hello Again World!");
+// AND &&
+let someShortCircuitLogicValue = (someMessage && someCoolMessage) && "Super cool short circuit logic string"; 
+console.log(someShortCircuitLogicValue);
+
+// OR ||
+let someOrCircuitMessage = someMessage || someCoolMessage || "super cool OR operator logic string"
+//                           "banana"          false                       ^^^
+console.log(someOrCircuitMessage);
